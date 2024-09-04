28f356356aa0f13f4e02e8cac1121d1863d83ce6
arianhassani <arianhassanimisc@gmail.com>
2024-09-04 12:40:55 +1000

    first commit

---
+++ nba-app/client/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='nav'>
+        <div className="nav-logo">NBA Analysis</div>
+        <ul className="nav-menu">
+            <li>Home</li>
+            <li>Explore</li>
+            <li>About</li>
+            <li className='nav-contact'>Contact</li>
+        </ul>
+
+    </div>
+  )
+}
+
+export default Navbar
