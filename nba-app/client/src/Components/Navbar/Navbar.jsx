9b28b571ce3f0d44600e9a3d4cbc8d9141c71ded
AnthonyNguyenTran <anthonytran.nguyen1@gmail.com>
2024-09-08 20:28:13 +1000

    created navbar and new empty pages

--- nba-app/client/src/Components/Navbar/Navbar.jsx
+++ nba-app/client/src/Components/Navbar/Navbar.jsx
@@ -1,17 +1,30 @@
+import React, { useState } from 'react';
 import './Navbar.css'
 
 const Navbar = () => {
-  return (
-    <div className='nav'>
-        <div className="nav-logo">NBA Analysis</div>
-        <ul className="nav-menu">
-            <li>Home</li>
-            <li>Explore</li>
-            <li>About</li>
-            <li className='nav-contact'>Contact</li>
-        </ul>
 
-    </div>
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className={`nav-links ${isOpen ? "active" : ""}`}>
+          <div className="nav-logo">NBA Analysis</div>
+              <a href="#home">Home</a>
+              <a href="#explore">Explore</a>
+              <a href="#about">About</a>
+              <a href="#contact">Contact</a>
+      </div>
+      <div className="hamburger" onClick={toggleMenu}>
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+      </div>
+    </nav>
+
   )
 }
 
