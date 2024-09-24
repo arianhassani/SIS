add024a94d3115f51ad14749888adcc4ef171800
Tarun Raja <tarun@Taruns-MacBook-Air.local>
2024-09-24 20:43:50 +1000

    test

--- nba-app/client/src/App.jsx
+++ nba-app/client/src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import TeamSelectionPage from "./pages/TeamSelectionPage";
 import InjuryPage from "./pages/InjuryPage";
+import HomePage from "./pages/HomePage";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Router>
         <Navbar />
         <Routes>
+          <Route path="/" element={<HomePage />} />
           <Route path="/team-selection" element={<TeamSelectionPage />} />
           <Route path="/injury" element={<InjuryPage />} />
         </Routes>
