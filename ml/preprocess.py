8404820450dfb94a739e47278265faf3c8b46f2c
Lukas Gaspar <zenikyn@gmail.com>
2024-10-20 13:15:14 +1100

    implement some new features including opponent id, is_home, day of week, add wl to unkown categoricals, seasonid to static categoricals

--- ml/preprocess.py
+++ ml/preprocess.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import yaml
 import os
+import typing as tp
 
 with open(os.path.join(__package__, "config.yaml"),"r") as file_object:
     config=yaml.load(file_object, Loader=yaml.SafeLoader)
@@ -14,32 +15,51 @@ def _read_games() -> pd.DataFrame:
 
     return games_df
 
-def preprocess() -> pd.DataFrame:
-    # preprocessed_path = os.path.join(config['data_dir'], 'preprocessed.csv')
-    # games_df.to_csv(preprocessed_path, index=False)
-    # games_df = pd.read_csv(preprocessed_path)
-
-    games_df: pd.DataFrame = _read_games()
+def preprocess(start: int = None, end: int = None) -> pd.DataFrame:
+    # win streak
     
+    games_df: pd.DataFrame = _read_games()
+
+    if start:
+        start = start % 100
+        condition = games_df['SEASON_ID'].mod(100) >= start
+        if end:
+            end = end % 100
+            condition &= games_df['SEASON_ID'].mod(100) <= end
+
+        games_df = games_df[condition]
+
     games_df['GAME_DATE'] = pd.to_datetime(games_df['GAME_DATE'])
-    games_df['WL'] = games_df['WL'].astype('category')
+    games_df['WL'] = games_df['WL'].replace({'W': '1', 'L': '0'}).astype('category')
     games_df['TEAM_ID'] = games_df['TEAM_ID'].astype(str).astype('category')
 
-    games_df = games_df.sort_values(['TEAM_ID', 'SEASON_ID', 'GAME_DATE'])
+    games_df['is_home'] = (~games_df['MATCHUP'].str.contains('@')).astype(int).astype(str).astype('category')
+    games_df['SEASON_ID'] = games_df['SEASON_ID'].astype(str).astype('category')
+
+    def assign_opponent(group):
+        group['OPPONENT_ID'] = group['TEAM_ID'].iloc[::-1].values
+            
+        return group
+
+    games_df = games_df.groupby('GAME_ID').apply(assign_opponent).reset_index(drop=True)
+
+    print((games_df['is_home'].astype(int) == games_df['WL'].astype(int)).sum() / len(games_df))
+
+    # games_df = games_df.sort_values(['TEAM_ID', 'SEASON_ID', 'GAME_DATE'])
 
     games_df['time_idx']  = games_df.groupby(['TEAM_ID', 'SEASON_ID']).cumcount()
 
-    games_df['day_of_week'] = games_df['GAME_DATE'].dt.day_of_week
+    games_df['day_of_week'] = games_df['GAME_DATE'].dt.day_of_week.astype(str).astype('category')
 
     games_df = games_df.drop(columns=['TEAM_ABBREVIATION', 'TEAM_NAME',\
                             'MATCHUP', 'VIDEO_AVAILABLE'], errors='ignore')
     games_df = games_df.dropna()
-    games_df.info()
+
     
     return games_df
 
 
 if __name__ == '__main__':
-    preprocess()
+    preprocess(2022)
 
 
