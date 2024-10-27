e60471d007e1379da1c516936eac8c9e15c98bac
Lukas Gaspar <zenikyn@gmail.com>
2024-10-27 21:11:02 +1100

    new best model

--- ml/predict.py
+++ ml/predict.py
@@ -8,6 +8,7 @@ import logging
 import os
 from .setup_data import setup_data
 import torch
+import torch.nn.functional as F
 
 logger = logging.getLogger(__name__)
 
@@ -42,21 +43,40 @@ class NBA_Pred_Model:
       'year': encoder_data.iloc[-1]['year'],
       'WL': '0' # needs to be filled but is irrelevant in decoding
     }
+    # print('opp rolling avg: ', decoder_dict['opponent_WL_rolling_avg'])
+    # encoder_data = self.all_teams_df
     encoder_data = encoder_data.drop(columns=['WL_int', 'GAME_ID'])
     decoder_data = pd.DataFrame(decoder_dict)
     prediction_data = pd.concat([encoder_data, decoder_data], ignore_index=True)
-    
-    y_pred_t: torch.Tensor = self.model.predict(prediction_data)
-    y_pred = y_pred_t[-1].item()
+    prediction_data.info()
+    # quantiles = self.model.predict(prediction_data, mode="quantiles")
+    y_pred = self.model.predict(prediction_data).item()
+    y_pred = 0.2 + y_pred * 0.6 # smooth odds
+    # y_pred = self.smooth_odds(quantiles)
+    # print(y_pred.prediction.output)    # probabilities = F.softmax(y_pred_t.output, dim=-1)
+    # probabilities = F.softmax(y_pred_t.prediction, dim=-1)
+    # print(y_pred_t)
+    # y_pred = y_pred_t[-1].item()
     logger.info(f'{self.teams[home]["nickname"]} vs {self.teams[away]["nickname"]} odds: {y_pred}')
     
     return y_pred
 
+  def smooth_odds(self, quantiles):
+    return sum(quantiles[0, 0]).item() / 7
+
+
 if __name__ == '__main__':
   config = setup_data(start_year=2023, end_year=2024, is_train=False)
   model = NBA_Pred_Model(**config)
-  model.predict({ 'home': {'teamId': '1610612738'}, 'away': {'teamId': '1610612761'}})
-  model.predict({ 'home': {'teamId': '1610612761'}, 'away': {'teamId': '1610612738'}})
+  res = []
+  team_ids = list(model.teams.keys())
+  n = len(team_ids)
+  n = 5
+  for i in range(n):
+    for j in range(i + 1, n):
+      res.append(model.predict({ 'home': {'teamId': team_ids[i]}, 'away': {'teamId': team_ids[j]}}))
+    # res.append(model.predict({ 'home': {'teamId': '1610612738'}, 'away': {'teamId': '1610612761'}}))
+    # res.append(model.predict({ 'home': {'teamId': '1610612748'}, 'away': {'teamId': '1610612738'}}))
 
-  
+  print(res)
   
