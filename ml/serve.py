16d2de74fe6850339ee0b97e0cada8c1f9362f2a
Henok Hailu <hen.hail33@gmail.com>
2024-10-29 17:11:50 +1100

    updated flask routing

--- ml/serve.py
+++ ml/serve.py
@@ -3,7 +3,7 @@ from flask import request, make_response
 from .setup_data import setup_data
 from .predict import NBA_Pred_Model, NBA_Pred_Team_Only_Model
 
-from .rfc_model import process_request
+from .rfc_model import RandomClassifierPrediction
 
 app = Flask(__name__)
 
@@ -11,11 +11,12 @@ config = setup_data(start_year=2023, end_year=2024)
 tft = NBA_Pred_Model(**config)
 tft_team_only = NBA_Pred_Team_Only_Model(**config)
 
+
 @app.post('/predict')
 def predict():
     data = request.get_json()
     return make_response({
         "tft": tft.predict(data),
         'tft_team_only': tft_team_only.predict(data),
-        'random_classifier' : process_request(data)
+        'random_classifier' : RandomClassifierPrediction.process_request(data)
         })
